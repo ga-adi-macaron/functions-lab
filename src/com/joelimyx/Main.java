7a2853b95172901758eb3279988e549e1ab6549a
Joe Lim <crazyfrogjoelim@gmail.com>
2016-10-04 16:42:17 -0400

    initial clone commit

---
+++ src/com/joelimyx/Main.java
@@ -0,0 +1,14 @@
+package com.joelimyx;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static void stringLength(String phrase){
+        System.out.println(phrase.length());
+    }
+    public static boolean isEven(int num1){
+        return ((num1%2)==0);
+    }
+}
